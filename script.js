bf5b3f5b35ade7298ca42ccca478eeea008ded28
HunterBoy344 <66140471+HunterBoy344@users.noreply.github.com>
2022-08-03 16:47:47 -0400

    Added files from Replit

---
+++ script.js
@@ -0,0 +1,239 @@
+var ws
+var input = document.getElementById('usernameinput')
+var input2 = document.getElementById('serverinput')
+var connectbutton = document.getElementById("connectbutton");
+var inputFocused = false
+var username
+var ID
+var trackName
+let leaderboardFrame = $("#leaderboard").contents().find('body');
+
+connectbutton.addEventListener("click", function(event) {
+  FS.unlink('/BERNIES.TRK')
+  FS.unlink('/CHERRIS.TRK')
+  FS.unlink('/DEFAULT.TRK')
+  FS.unlink('/HELENS.TRK')
+  FS.unlink('/JOES.TRK')
+  FS.unlink('/SKIDS.TRK')
+  FS.unlink('/BERNIES.HIG')
+  FS.unlink('/CHERRIS.HIG')
+  FS.unlink('/DEFAULT.HIG')
+  FS.unlink('/HELENS.HIG')
+  FS.unlink('/JOES.HIG')
+  FS.unlink('/SKIDS.HIG')
+  connectbutton.disabled = true;
+  connectbutton.innerHTML = 'Connected!'
+  let server = input2.value
+  username = input.value
+  input.value = ''
+  input2.value = ''
+  console.log('Attempting connection to WebSocket server...')
+  ws = new WebSocket(`wss://${server}`)
+  ws.binaryType = "arraybuffer";
+  ws.addEventListener('open', (event) => {
+    console.log('Connection was successful!')
+    ws.send(`{ "username" : "${username}" }`);
+    var checkForReplayInterval = setInterval(getReplayFromFS, 1000);
+    ws.addEventListener('message', event => {
+      var response = event.data
+      if (response instanceof ArrayBuffer) {
+        var view = new Uint8Array(response);
+        console.log(view)
+        FS.writeFile(`/${trackName}.TRK`, view)
+        var x = document.getElementById("warning");
+        x.style.display = "none";
+        return
+      }
+      var responseJSON = JSON.parse(response)
+      if (responseJSON.hasOwnProperty('timer')) {
+        // var doc = document.getElementById('timer').contentWindow.document;
+        // doc.open();
+        // doc.write(responseJSON.timer);
+        // doc.close();
+        let remainingTime = sToTime(responseJSON.timer)
+        $("#timer").html(remainingTime);
+      }
+      if (responseJSON.hasOwnProperty('leaderboard')) {
+        var leaderboard = responseJSON.leaderboard
+        // var doc = document.getElementById('leaderboard').contentWindow.document;
+        // doc.open();
+        $("#leaderboard").html(``);
+        leaderboard.forEach((element, index) => {
+          if (element.time === 999999) {
+            // doc.write(`#${index + 1}: ${element.username} (???)<br>`);
+            $("#leaderboard").append(`#${index + 1}: ${element.username} (???)<br>`);
+          } else {
+            //doc.write(`#${index + 1}: ${element.username} (${msToTime(element.time/**50*/)})<br>`);
+            $("#leaderboard").append(`#${index + 1}: ${element.username} (${msToTime(element.time/**50*/)})<br>`);
+          }
+        });
+        // doc.close();
+      }
+      if (responseJSON.hasOwnProperty('warning')) {
+        var x = document.getElementById("warning");
+        x.style.display = "block";
+        FS.unlink(`/${trackName}.TRK`)
+      }
+      if (responseJSON.hasOwnProperty('ID')) {
+        console.log(responseJSON)
+        ID = responseJSON.ID
+      }
+      if (responseJSON.hasOwnProperty('track')) {
+        let buffer = _base64ToArrayBuffer(responseJSON.track)
+        let view = new Uint8Array(buffer);
+        console.log(view)
+        trackName = responseJSON.name
+        trackName = trackName.replace(/\.[^/.]+$/, "")
+        FS.writeFile(`/${trackName}.TRK`, view)
+        var x = document.getElementById("warning");
+        x.style.display = "none";
+        return
+      }
+    });
+  });
+  ws.addEventListener('close', event => {
+    console.error(`Connection closed by the server.`)
+  })
+});
+
+function getReplayFromFS() {
+  /* var replayCheck = FS.analyzePath('/DEFAULT.RPL')
+  if (replayCheck.exists == true) {
+    var replay = FS.readFile('/DEFAULT.RPL', { encoding : 'binary' })
+    ws.send(`{"replay" : "${_arrayBufferToBase64(replay.buffer)}", "username" : "${username}", "ID" : "${ID}"}`)
+    console.log('Automatically sent replay!')
+    FS.unlink('/DEFAULT.RPL')
+  } */
+  var replayCheck = FS.analyzePath(`/${trackName}.HIG`)
+  if (replayCheck.exists == true) {
+    var replay = FS.readFile(`/${trackName}.HIG`, { encoding : 'binary' })
+    ws.send(`{"replay" : "${_arrayBufferToBase64(replay.buffer)}", "username" : "${username}", "ID" : "${ID}" }`)
+    console.log('Automatically sent replay!')
+    FS.unlink(`/${trackName}.HIG`)
+  }
+}
+
+function msToTime(s) {
+    function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
+    }
+
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    return pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
+}
+
+function sToTime(s) {
+    function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
+    }
+
+    s = s * 1000
+
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    return pad(mins) + ':' + pad(secs)
+}
+
+function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
+
+function _base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+function simulateKeyEvent(charCode, pressed) 
+{
+    var name = pressed ? "keydown" : "keyup";
+    var event = document.createEvent('KeyboardEvent');
+    var getter = {
+        get: function() { return this.keyCodeVal; }
+    };
+
+    if(!element) return;
+
+    // Chromium Hack
+    Object.defineProperty(event, 'keyCode', getter);
+    Object.defineProperty(event, 'which', getter);
+    Object.defineProperty(event, 'charCode', getter);   
+
+    if(event.initKeyboardEvent) 
+    {
+        event.initKeyboardEvent(name, true, true, document.defaultView, false, false, false, false, charCode, charCode);
+    } 
+    else 
+    {
+        event.initKeyEvent(name, true, true, document.defaultView, false, false, false, false, charCode, 0);
+    }
+
+    event.keyCodeVal = charCode;
+
+    if(event.keyCode !== charCode) 
+    {
+        alert("keyCode mismatch " + event.keyCode + "(" + event.which + ")");
+    }
+
+    element.dispatchEvent(event);
+}
+
+
+window.addEventListener('keydown', function(event){
+  if (inputFocused == true) {
+    event.stopImmediatePropagation();
+  }
+}, true);
+
+window.addEventListener('keyup', function(event){
+  if (inputFocused == true) {
+    event.stopImmediatePropagation();
+  }
+}, true);
+
+/* window.addEventListener('beforeunload', (event) => {
+  if (!ws) return
+  ws.send(`{ "disconnect" : "me when amogus", "username" : "${username}", "ID" : "${ID}" }`)
+}); */
+
+window.onbeforeunload = function () {
+  if (!ws) return
+  ws.send(`{ "disconnect" : "me when amogus", "username" : "${username}", "ID" : "${ID}" }`)
+};
+
+input.addEventListener('focus', (event) => {
+  inputFocused = true
+}, true);
+input2.addEventListener('focus', (event) => {
+  inputFocused = true
+}, true);
+
+input.addEventListener('blur', (event) => {
+  inputFocused = false
+}, true);
+input2.addEventListener('blur', (event) => {
+  inputFocused = false
+}, true);
